d6a17f1c9fa61e1dd46d4475d7a3bbcb591a7944
Svenhildr <julie.poissonnier.1989@gmail.com>
2023-05-12 09:52:06 +0200

    mise à jour cards

--- react-frontend-dev-portfolio/src/components/Projects.js
+++ react-frontend-dev-portfolio/src/components/Projects.js
@@ -25,14 +25,18 @@ class Projects extends Component {
             key={projects.title}
             style={{ cursor: "pointer" }}
           >
-            <span className="portfolio-item d-block">
+            <span className="portfolio-item">
               <div className="foto" onClick={() => detailsModalShow(projects)}>
                 <div>
                   <img
                     src={projects.images[0]}
                     alt="projectImages"
                     height="230"
-                    style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
+                    style={{
+                      marginBottom: 0,
+                      paddingBottom: 0,
+                      position: "relative",
+                    }}
                   />
                   <span className="project-date">{projects.startDate}</span>
                   <br />
